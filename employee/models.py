e39fe1a490e5c5d493492d196261f35c1376c060
kartiksingalkar <kartiksingalkar6@gamail.com>
2022-07-26 23:28:11 +0530

    first commit

---
+++ employee/models.py
@@ -0,0 +1,16 @@
+import email
+from django.db import models
+
+# Create your models here.
+class Employee(models.Model):
+    eid=models.CharField(max_length=50)
+    ename=models.CharField(max_length=100)
+    eemail=models.EmailField()
+    econtact=models.CharField(max_length=15)
+    
+    def __str__(self):
+        return "%s" %(self.ename)
+    
+    class Meta:
+        db_table="employee"
+        
